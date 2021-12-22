d2f632ddf3c5a7b112657ec2e58d1406a7268514
KIsick1482 <juliasavitscaya@gmail.com>
2021-12-22 18:57:09 +0300

    Adding a playing field. Changing object control.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,30 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.cube = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.cube)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // cube
             // 
             this.cube.BackColor = System.Drawing.Color.Cyan;
-            this.cube.Location = new System.Drawing.Point(222, 192);
+            this.cube.Location = new System.Drawing.Point(20, 20);
             this.cube.Name = "cube";
             this.cube.Size = new System.Drawing.Size(20, 20);
             this.cube.TabIndex = 0;
             this.cube.TabStop = false;
             this.cube.Click += new System.EventHandler(this.cube_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(20, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 1);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox2.Location = new System.Drawing.Point(20, 520);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(500, 1);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox3.Location = new System.Drawing.Point(20, 20);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(1, 500);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox4.Location = new System.Drawing.Point(520, 20);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(1, 500);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Desktop;
-            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.ClientSize = new System.Drawing.Size(682, 553);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.cube);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.cube)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -59,6 +120,11 @@
         #endregion
 
         private System.Windows.Forms.PictureBox cube;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,32 +12,89 @@ namespace Snake
 {
     public partial class Form1 : Form
     {
+        private int dirX, dirY;
+        private int _width = 700;
+        private int _height = 600;
+        private int _sizeOfSides = 20;
         public Form1()
         {
             InitializeComponent();
+            this.Width = _width;
+            this.Height = _height;
+            dirX = 1;
+            dirY = 0;
+            _generateMap();
+            timer1.Tick += new EventHandler(_update);
+            timer1.Interval = 500;
+            timer1.Start();
             this.KeyDown += new KeyEventHandler(OKP);
         }
-            private void OKP(object sender, KeyEventArgs e)
+        private void _generateMap()
+        {
+            for(int i = 1; i <= _width / 28; i++)
             {
-                MessageBox.Show(e.KeyCode.ToString());
+                PictureBox picb = new PictureBox();
+                picb.BackColor = Color.DarkGreen;
+                picb.Location = new Point(20, _sizeOfSides * i);
+                picb.Size = new Size(_width - 200, 1);
+                this.Controls.Add(picb);
+            }
+            for (int i = 1; i < _height / 23; i++)
+            {
+                PictureBox picb = new PictureBox();
+                picb.BackColor = Color.DarkGreen;
+                picb.Location = new Point( _sizeOfSides * i, 20);
+                picb.Size = new Size(1, _height - 100);
+                this.Controls.Add(picb);
+            }
+        }
+        private void _update(object myObject, EventArgs eventArgs)
+        {
+            cube.Location = new Point(cube.Location.X + dirX * _sizeOfSides, cube.Location.Y + dirY * _sizeOfSides);
+        }
+        private void OKP(object sender, KeyEventArgs e)
+            {
                 switch (e.KeyCode.ToString())
                 {
                     case "Right":
-                       cube.Location = new Point(cube.Location.X + 20, cube.Location.Y);
-                       break;
-                    case "Left":
-                        cube.Location = new Point(cube.Location.X - 20, cube.Location.Y);
+                        dirX = 1;
+                        dirY = 0;
                         break;
-
+                    case "Left":
+                        dirX = -1;
+                        dirY = 0;
+                        break;
                     case "Up":
-                        cube.Location = new Point(cube.Location.X, cube.Location.Y - 20);
+                        dirX = 0;
+                        dirY = -1;
                         break;
                     case "Down":
-                        cube.Location = new Point(cube.Location.X, cube.Location.Y + 20);
+                        dirX = 0;
+                        dirY = 1;
                         break;
             }
         }
             private void cube_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
         {
 
         }
