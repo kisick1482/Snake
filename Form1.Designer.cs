c518b89f592bbc6be3072baf0977756465f8a8ad
Kisick <Kisick@MASHINA>
2021-12-22 15:55:30 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cube = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cube)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cube
+            // 
+            this.cube.BackColor = System.Drawing.Color.Cyan;
+            this.cube.Location = new System.Drawing.Point(222, 192);
+            this.cube.Name = "cube";
+            this.cube.Size = new System.Drawing.Size(20, 20);
+            this.cube.TabIndex = 0;
+            this.cube.TabStop = false;
+            this.cube.Click += new System.EventHandler(this.cube_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(482, 453);
+            this.Controls.Add(this.cube);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.cube)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox cube;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(OKP);
+        }
+            private void OKP(object sender, KeyEventArgs e)
+            {
+                MessageBox.Show(e.KeyCode.ToString());
+                switch (e.KeyCode.ToString())
+                {
+                    case "Right":
+                       cube.Location = new Point(cube.Location.X + 20, cube.Location.Y);
+                       break;
+                    case "Left":
+                        cube.Location = new Point(cube.Location.X - 20, cube.Location.Y);
+                        break;
+
+                    case "Up":
+                        cube.Location = new Point(cube.Location.X, cube.Location.Y - 20);
+                        break;
+                    case "Down":
+                        cube.Location = new Point(cube.Location.X, cube.Location.Y + 20);
+                        break;
+            }
+        }
+            private void cube_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
